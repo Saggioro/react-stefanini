2aebec72e2bb7c5f503281c946d76ec44eac8c19
Saggioro <diegosaggiorobrito@gmail.com>
2022-07-17 22:53:10 -0300

    feat: add route file

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Login from "../pages/login/Login";
-//import ControleRedirecionamento from 'pages/infra/ControleRedirecionamento';
+import Home from "../pages/home/Home";
 
 export const Router: React.FunctionComponent = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   );
 };
