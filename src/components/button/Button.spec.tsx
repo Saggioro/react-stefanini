00afe8709febac2eb0cebfe6fc82a2e870bca576
Saggioro <diegosaggiorobrito@gmail.com>
2022-07-18 11:49:15 -0300

    test: add button tests

--- src/components/button/Button.spec.tsx
+++ src/components/button/Button.spec.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable testing-library/no-debugging-utils */
-import { render } from "@testing-library/react";
 import renderer from 'react-test-renderer';
 import Button from "./Button";
 
