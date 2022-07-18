d9a9817cda11245efa382ec4336b0c0b72d62943
Saggioro <diegosaggiorobrito@gmail.com>
2022-07-17 22:49:39 -0300

    feat: add user and auth reducer to redux

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,13 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import { persistStore, persistCombineReducers } from "redux-persist";
 import { PersistGate } from "redux-persist/es/integration/react";
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ReduxThunk from "redux-thunk";
 import { Router } from "./routes/Routes";
 
 //reducers
+import UsersReducer from "./store/reducers/users";
+import AuthReducer from "./store/reducers/auth";
 //PERSIST
 const persistConfig = {
   key: "root",
@@ -20,6 +22,8 @@ const persistConfig = {
 };
 
 const persistedCombinedReducer = persistCombineReducers(persistConfig, {
+  usersReducer: UsersReducer,
+  authReducer: AuthReducer,
 });
 
 const reduxStore = createStore(
