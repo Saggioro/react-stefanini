86994a86541aec6c0129e150932ba23e45c7037c
Saggioro <diegosaggiorobrito@gmail.com>
2022-07-18 09:32:28 -0300

    chore: add jest and configs

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+module.exports = {
+  testPathIgnorePatterns: ["/node_modules/", "/cypress/"],
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$":"<rootDir>/node_modules/babel-jest"
+  },
+  testEnvironment: "jsdom",
+  collectCoverage: true,
+  collectCoverageFrom: [
+      "src/**/*.{tsx}",
+      "!src/**/*.spec.{tsx}",
+      "!**/node_modules/**",
+      "!**/vendor/**"
+  ],
+  coverageReporters: ['clover', 'json', 'lcov'],
+};
