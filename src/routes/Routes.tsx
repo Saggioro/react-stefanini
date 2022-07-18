17c03ac910ac1df0ed3b29650758f82203cc9502
Saggioro <diegosaggiorobrito@gmail.com>
2022-07-18 09:36:04 -0300

    feat: add redirect when not logged or logged

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,14 +1,33 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import {
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 
 import Login from "../pages/login/Login";
 import Home from "../pages/home/Home";
+import { useAppSelector } from "../hooks/LocalReduxThunk";
 
 export const Router: React.FunctionComponent = () => {
+  const user = useAppSelector((state) => state.authReducer.loggedUser);
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user && pathname === "/") {
+      navigate("/home");
+    } else if (!user && pathname !== "/") {
+      navigate("/");
+    }
+    
+  }, [user, pathname]);
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/home" element={<Home />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 };
